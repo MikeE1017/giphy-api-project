313ea286af6eb8c8561588101edfb215096db6c0
MikeE1017 <mjelovesmge.101706@gmail.com>
2025-10-29 22:31:38 -0500

    added functions to fetch the GIFs and pull the images from the URLs. Added fetch button with a click listener.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,31 @@ const apiKey = "fiQUcXaLjs7reP13he9SCxN1HfHDwaad";
 
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=fiQUcXaLjs7reP13he9SCxN1HfHDwaad&q=graduation&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
 
+async function fetchGifs() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data); //look what comes back
+}
+
+async function fetchGifs() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  const images = data.data.map(gif => gif.images.original.url);
+  console.log(images); // test that it shows image URLs
+}
+
+const gifContainer = document.querySelector("#gif-container");
+const fetchButton = document.querySelector("#fetch-gif-btn");
+
+fetchButton.addEventListener("click", async () => {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  const images = data.data.map(gif => gif.images.original.url);
+
+  gifContainer.innerHTML = ""; // clear previous GIFs
+
+  for (let img of images) {
+    gifContainer.innerHTML += `<img src="${img}" class="col-3 mb-3">`;
+  }
+});
