04d553136e9ecfe72519d38164da3a987e35b48d
MikeE1017 <mjelovesmge.101706@gmail.com>
2025-10-29 21:53:42 -0500

    copied over api key and request url

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("script.js loaded");
+
+const apiKey = "fiQUcXaLjs7reP13he9SCxN1HfHDwaad";
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=fiQUcXaLjs7reP13he9SCxN1HfHDwaad&q=graduation&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
